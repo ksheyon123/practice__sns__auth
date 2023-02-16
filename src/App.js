fae92d0a7f545f7af83788aef5b373dda06e1753
Kangseohyeon <kshyeon123@gmail.com>
2023-02-16 18:40:20 +0900

    Fixing twitter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useCallback } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { queryParser } from "./utils/index";
-import { requestAuthToken } from "./apis/api";
+import { requestAuthToken, requestTweeterAuthToken } from "./apis/api";
 
 const Page = () => {
   const location = window.location;
   const domain = location.origin;
   console.log(domain);
   const { search } = useLocation();
-  const { code } = queryParser(search);
-
+  const { code, state } = queryParser(search);
+  console.log(state);
   const getDiscordAuthToken = useCallback(async () => {
     try {
-      console.log(domain);
       const result = await requestAuthToken({
         code,
         redirect_uri: domain,
@@ -23,11 +22,26 @@ const Page = () => {
     }
   }, [code, domain]);
 
+  const getTweeterAuthToken = useCallback(async () => {
+    try {
+      const result = await requestTweeterAuthToken({
+        code,
+      });
+      console.log(result);
+    } catch (e) {
+      throw e;
+    }
+  }, [domain, code]);
+
   useEffect(() => {
     if (!!code) {
       getDiscordAuthToken();
+      getTweeterAuthToken();
     }
   }, [code]);
+
+  useEffect(() => {}, []);
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <div>
@@ -39,6 +53,18 @@ const Page = () => {
           }}
         >
           Login To Discord
+        </button>
+      </div>
+      <div>
+        <div>Tweeter</div>
+        <button
+          onClick={() => {
+            const encodedURL = encodeURIComponent("http://10.211.0.130:3000");
+            window.location = `https://twitter.com/i/oauth2/authorize?response_type=code&client_id=ZDdOa0RjWlJYLWZQUHFHa0tCMmw6MTpjaQ&redirect_uri=${encodedURL}&scope=tweet.read%20users.read%20follows.read%20offline.access&state=state&code_challenge=challenge&code_challenge_method=plain`;
+            // getTweeterAuthToken();
+          }}
+        >
+          Login To Tweeter
         </button>
       </div>
     </div>
--- src/apis/api.js
+++ src/apis/api.js
@@ -1,16 +1,34 @@
 import { queryStringCreator } from "../utils/index";
+
 const DISCORD_ENDPOINT = "https://discord.com";
+const TWITTER_ENDPOINT = "https://api.twitter.com";
 const DISCORD_CLIENT_ID = process.env.REACT_APP_DISCORD_CLIENT_ID;
 const DISCORD_CLIENT_SECRET = process.env.REACT_APP_DISCORD_CLIENT_SECRET;
+const TWITTER_CLIENT_ID = process.env.REACT_APP_TWITTER_CLIENT_ID;
+const TWITTER_CLIENT_SECRET = process.env.REACT_APP_TWITTER_CLIENT_SECRET;
+const TWITTER_BEARER_TOKEN = process.env.REACT_APP_TWITTER_BEARER_TOKEN;
 const v_1 = "1.1";
 const v_2 = "2.0";
 const PATH = {
-  oauth_2: "/api/oauth2/token",
+  discord: {
+    oauth_2: "/api/oauth2/token",
+  },
+  twitter: {
+    oauth_2: "/oauth2/token",
+  },
 };
 const DISCORD_AUTH_PARAMS = {
   client_id: DISCORD_CLIENT_ID,
   client_secret: DISCORD_CLIENT_SECRET,
   grant_type: "authorization_code",
+};
+
+const TWITTER_AUTH_PARAMS = {
+  client_id: TWITTER_CLIENT_ID,
+  client_secret: TWITTER_CLIENT_SECRET,
+  grant_type: "authorization_code",
+  code_verifier: "challenge",
+  redirect_uri: "http://10.211.0.130:3000",
 };
 
 const requestAuthToken = async (params) => {
@@ -19,10 +37,41 @@ const requestAuthToken = async (params) => {
       ...DISCORD_AUTH_PARAMS,
       ...params,
     };
-    console.log(authParams);
-    await post(`https://discord.com/api/oauth2/token`, {
-      ...authParams,
-    });
+    console.log("Discord", authParams);
+    await post(
+      `${DISCORD_ENDPOINT}${PATH.discord.oauth_2}`,
+      {
+        ...authParams,
+      },
+      {}
+    );
+  } catch (e) {
+    throw e;
+  }
+};
+
+const requestTweeterAuthToken = async (params, queryString) => {
+  try {
+    const authParams = {
+      ...TWITTER_AUTH_PARAMS,
+      ...params,
+    };
+
+    console.log(
+      "Twitter",
+      btoa(`${TWITTER_CLIENT_ID}:${TWITTER_CLIENT_SECRET}`)
+    );
+    const rsp = await post(
+      `${TWITTER_ENDPOINT}${PATH.twitter.oauth_2}`,
+      { ...authParams },
+      {},
+      {
+        Authorization: `Basic ${btoa(
+          `${TWITTER_CLIENT_ID}:${TWITTER_CLIENT_SECRET}`
+        )}`,
+      }
+    );
+    console.log(rsp);
   } catch (e) {
     throw e;
   }
@@ -36,8 +85,7 @@ const get = async (domain, params) => {
       method: "GET",
       headers: {
         ...headers,
-        Accept: "application/json",
-        "Content-Type": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
     });
     if (rsp.ok) {
@@ -51,20 +99,17 @@ const get = async (domain, params) => {
   }
 };
 
-const post = async (domain, params) => {
+const post = async (domain, params, queryString, headers) => {
   try {
-    const headers = {};
-
     const param = new URLSearchParams();
     const keys = Object.keys(params);
     keys.map((el) => {
       param.append(el, params[el]);
     });
-    const rsp = await fetch(domain, {
+    const rsp = await fetch(`${domain}`, {
       method: "POST",
       headers: {
         ...headers,
-        Accept: "application/json",
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: param,
@@ -81,4 +126,4 @@ const post = async (domain, params) => {
   }
 };
 
-export { requestAuthToken };
+export { requestAuthToken, requestTweeterAuthToken };
